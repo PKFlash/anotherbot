ebfa3700d1997dca3ec565fad691f0b7554467eb
Norimune <richaagni78969@gmail.com>
2018-11-13 14:36:13 -0500

    commands and embed changes

---
+++ commands/commandsList.js
@@ -0,0 +1,17 @@
+const embed1 = new Discord.RichEmbed()
+  .setTitle("**IS THIS WHAT YOU WANT?!**")
+  .setAuthor("fuckfuckfuckfuckfuckfuck", "https://cdn.discordapp.com/attachments/500773668440375297/500817119865208863/fuck.JPG")
+  .setColor("#00f9ff")
+  .setDescription("available commands")
+  .setFooter("Next time he comes here, we're gonna GET his ass")
+  .setImage("https://cdn.discordapp.com/attachments/500773668440375297/500817119865208863/fuck.JPG")
+  .addField("Commands", "in progress")
+  .addField("Additional info", "Taking a ride to my old haunt \nshe's in my blood, she resides in my nightlife \nmy favorite color's candy apple girl");
+
+module.exports = {
+  name: 'commandsList',
+  description: 'list of commands, please work',
+  execute(message, args) {
+    message.channel.send({embed1});
+  },
+};
--- index.js
+++ index.js
@@ -21,12 +21,11 @@ const embed = new Discord.RichEmbed()
   .setTitle("**IS THIS WHAT YOU WANT?!**")
   .setAuthor("fuckfuckfuckfuckfuckfuck", "https://cdn.discordapp.com/attachments/500773668440375297/500817119865208863/fuck.JPG")
   .setColor("#00f9ff")
-  .setDescription("available commands")
+  .setDescription("All these landmines, so little time")
   .setFooter("Next time he comes here, we're gonna GET his ass")
   .setImage("https://cdn.discordapp.com/attachments/500773668440375297/500817119865208863/fuck.JPG")
   .addField("Landmines",
     "one minute \nknee \nnose \nnightlife \ndrink \nrichie dagger \ndagger.help")
-  .addField("Commands", "in progress")
   .addField("Additional info", "Taking a ride to my old haunt \nshe's in my blood, she resides in my nightlife \nmy favorite color's candy apple girl");
 
 //setup
