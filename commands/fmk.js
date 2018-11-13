250a26f87cc0eec3c205ed779090a9fbf98189ed
Norimune <richaagni78969@gmail.com>
2018-11-13 00:49:38 -0500

    fmk

--- commands/fmk.js
+++ commands/fmk.js
@@ -3,12 +3,7 @@ module.exports = {
   description: 'why did I make this',
   args: true,
   execute(message, args) {
-    if (!args.length === 3) {
-      return message.channel.send("Hey, you should know how this works by now.");
-    }
-
-    else {
-      return message.channel.send("test1");
-    }
+    let [first, second, third] = args;
+    message.reply(`first ${first}, second ${second}, third ${third}`);
   },
 };
