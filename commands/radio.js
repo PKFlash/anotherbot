76d1f6b89d29f301916ce66473b40c093d5bd63d
Norimune <richaagni78969@gmail.com>
2018-11-27 16:58:13 -0500

    Update radio.js
    better words

--- commands/radio.js
+++ commands/radio.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'radio',
   description: 'tune in!',
   execute(message, args) {
-    message.channel.send("Tune in at 10! Click 'Listen Live' and go for the Pulse of Music stream \nhttps://witr.rit.edu/");
+    message.channel.send("Tune in at 10! Click 'Listen Live' and go for the Pulse of Music stream. \nhttps://witr.rit.edu/\nThis is *Unsafe at Any Speed,* two hours of straight punk rock followed by a couple of random genres and a Queen song");
   },
 };
