be376fdf3e1c5080324ddd1c534615758d37009e
Norimune <richaagni78969@gmail.com>
2019-01-06 13:06:29 -0500

    Update align.js
    align

--- commands/align.js
+++ commands/align.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'align',
   description: 'server alignment chart',
   execute(message, args) {
-    message.channel.send("https://cdn.discordapp.com/attachments/487409340538421248/499316960421740545/memeshrine.png");
+    message.channel.send("https://cdn.discordapp.com/attachments/487409340538421248/531533792423182340/unknown.png");
   },
 };
