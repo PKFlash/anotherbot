3fd799417b0713e6fd968f961f7cfaadc3c2f57e
Norimune <richaagni78969@gmail.com>
2019-01-07 18:46:17 -0500

    Update index.js
    ????

--- index.js
+++ index.js
@@ -120,9 +120,12 @@ client.on("message", (message) => {
    }
 
 //commands using ! prefix, defined at the top outside any {}
+  const x = message.content.slice(prefix.length);
+  const commandName = x.substr(0, x.indexOf(" ")).toLowerCase();
+  const args = x.slice(commandName.length).split(", ");
 
-  const args = message.content.slice(prefix.length).split(", ");
-  const commandName = args.shift().toLowerCase();
+//const args = message.content.slice(prefix.length).split(", ");
+//const commandName = args.shift().toLowerCase();
 
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
