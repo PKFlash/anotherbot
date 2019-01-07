308c271553fa62cc9f1a9f276c7c37db7245eaef
Norimune <richaagni78969@gmail.com>
2019-01-07 13:52:41 -0500

    Update index.js
    take 2

--- index.js
+++ index.js
@@ -121,7 +121,8 @@ client.on("message", (message) => {
 
 //commands using ! prefix, defined at the top outside any {}
   const x = message.content.slice(prefix.length);
-  const commandName = x.substr(0, x.indexOf(" ")).shift().toLowerCase();
+  const y = x.substr(0, x.indexOf(" "));
+  const commandName = y.shift().toLowerCase();
   const args = x.slice(commandName.length);
 
 //const args = message.content.slice(prefix.length).split(", ");
