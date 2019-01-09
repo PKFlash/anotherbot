5428946653e0ad55b0c04bddf109bbfc9c0dad15
Norimune <richaagni78969@gmail.com>
2019-01-09 18:40:50 -0500

    error fix

--- commands/rate.js
+++ commands/rate.js
@@ -17,7 +17,7 @@ module.exports = {
       else if (strp.startsWith("my")) {
         var str3 = strp.substr(3);
         const yr0 = "your ";
-        var str4 = yr.concat(str3);
+        var str4 = yr0.concat(str3);
 
         if (rating === 8) {
           message.channel.send(`I rate ${str4} an 8 out of 10.`);
@@ -29,7 +29,7 @@ module.exports = {
       else if (strp.startsWith("your")) {
         var str5 = strp.substr(5);
         const yr1 = "my ";
-        var str6 = yr.concat(str5);
+        var str6 = yr1.concat(str5);
 
         if (rating === 8) {
           message.channel.send(`I rate ${str6} an 8 out of 10.`);
@@ -41,7 +41,7 @@ module.exports = {
       else if (strp === "me") {
         var str7 = strp.substr(3);
         const yr2 = "you ";
-        var str8 = yr.concat(str7);
+        var str8 = yr2.concat(str7);
 
         if (rating === 8) {
           message.channel.send(`I rate ${str8} an 8 out of 10.`);
