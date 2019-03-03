656b7d23c3974dea778505331db7ebd0e7c0780a
Norimune <richaagni78969@gmail.com>
2019-03-03 18:05:18 -0500

    test

--- commands/cycopedia.js
+++ commands/cycopedia.js
@@ -11,6 +11,31 @@ module.exports = {
 
   //Structure: !cycopedia [find, add, edit, remove]0, [year, tag, artist]1
 
+  if (args[0] === "add") {
+    if(message.member.roles.some(role => role.name === 'meme god')) {
+      if (args.length != 5){
+        message.channel.send("Input artist, album name, year, and tags (separate tags by spaces, not commas).");
+      }
+      else {
+        let [cmd, name, album, year, tags] = args;
+        client.cycopedia[message.author.username] = {
+          message: message.content
+        }
+
+        fs.writeFile("cycopedia.json", JSON.stringify(client.cycopedia, null, 4), err => {
+          if (err) throw err;
+          else {
+            message.channel.send("got it");
+          }
+        }
+        );
+      }
+    }
+    if(!message.member.roles.some(role => role.name === 'meme god')) {
+      return message.reply("Nice try.");
+    }
+  }
+
   if (args[0] === "find") {
     if (args.length != 2) {
       message.channel.send("What you're looking for is *find* and then *year*, *tag*, or *artist*." );
@@ -23,30 +48,5 @@ module.exports = {
     }
   }
 
-  if (args[0] === "add") {
-    if(message.member.roles.some(role => role.name === 'meme god')) {
-      if (args.length != 5){
-        message.channel.send("Input artist, album name, year, and tags (separate tags by spaces, not commas).");
-      }
-      else {
-        let [cmd, name, album, year, tags] = args;
-        client.cycopedia[message.author.username] = {
-          message: args[1]
-        }
-
-        fs.writeFile("cycopedia.json", JSON.stringify(client.cycopedia, null, 4), err => {
-          if (err) throw err;
-          else {
-            message.channel.send("got it");
-          }
-        }
-        );
-      }
-    }
-  }
-    //Add, delete, and edit commands
-    if(!message.member.roles.some(role => role.name === 'meme god')) {
-      return message.reply("Nice try.");
-    }
     },
 };
