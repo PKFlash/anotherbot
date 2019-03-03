6bd7171afe9f02f39023a042dd014aaa5729188c
Norimune <richaagni78969@gmail.com>
2019-03-03 15:21:07 -0500

    test

--- commands/cycopedia.js
+++ commands/cycopedia.js
@@ -6,25 +6,36 @@ module.exports = {
     var {client} = require("./../index.js");
     client.cycopedia = require("./../cycopedia.json");
 
-  if (args.length != 2) {
-    message.channel.send("What you're looking for is *find* and then *year*, *tag*, or *artist*." );
-  }
-  else {
-  let [first, second] = args;
+    var data = fs.readFileSync('./../cycopedia.json');
+    var words = JSON.parse(data);
+
+  //Structure: !cycopedia [find, add, edit, remove]0, [year, tag, artist]1
 
   if (args[0] === "find") {
-    message.channel.send("Input tags.");
+    if (args.length != 2) {
+      message.channel.send("What you're looking for is *find* and then *year*, *tag*, or *artist*." );
+    }
+    else {
+    let [first, second] = args;
+    console.log(words);
+    }
   }
 
   if (args[0] === "add") {
     if(message.member.roles.some(role => role.name === 'meme god')) {
-      message.channel.send("Test successful.");
+      if (args.length != 5){
+        message.channel.send("Input artist, album name, year, and tags (separate tags by spaces, not commas).");
+      }
+      else {
+        client.cycopedia[] = {
+        message:
+        }
+      }
     }
     //Add, delete, and edit commands
     if(!message.member.roles.some(role => role.name === 'meme god')) {
       return message.reply("Nice try.");
     }
-  }
   }
     },
 };
