6e57478c220ca822aea71564f128bb63aa8fcbbc
Norimune <richaagni78969@gmail.com>
2019-03-04 18:51:48 -0500

    post

--- commands/embed.js
+++ commands/embed.js
@@ -9,7 +9,8 @@ const embed = new Discord.RichEmbed()
   .setFooter("Next time he comes here, we're gonna GET his ass")
   .setImage("https://cdn.discordapp.com/attachments/500773668440375297/500817119865208863/fuck.JPG")
   .addField("Landmines",
-    "one minute \nknee \nnose \nnightlife \ndrink \nrichie dagger \ndagger.help")
-  .addField("Additional info", "Taking a ride to my old haunt \nshe's in my blood, she resides in my nightlife \nmy favorite color's candy apple girl");
+    "one minute \nknee \nnose \nnightlife \ndrink \nelement \nrichie dagger")
+  .addField("Additional info", "Taking a ride to my old haunt \nshe's in my blood, she resides in my nightlife \nmy favorite color's candy apple girl")
+  .addField("Ask me questions, I fucking dare you!");
 
 module.exports = (embed);
