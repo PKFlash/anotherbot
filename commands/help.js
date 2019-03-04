34d105399ea5552709bb0415ffe403614b84fc1d
Norimune <richaagni78969@gmail.com>
2019-03-04 18:19:56 -0500

    pls

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    name: 'help',
-    description: 'List all of my commands or info about a specific command.',
-    aliases: ['commands'],
-    execute(message, args) {
+
       var hEmbed = new Discord.RichEmbed()
         .setTitle("**IS THIS WHAT YOU WANT?!**")
         .setAuthor("fuckfuckfuckfuckfuckfuck", "https://cdn.discordapp.com/attachments/500773668440375297/500817119865208863/fuck.JPG")
@@ -16,8 +13,12 @@ module.exports.run = async (bot, message, args) => {
           "one minute \nknee \nnose \nnightlife \ndrink \nrichie dagger \ndagger.help")
         .addField("Additional info", "Taking a ride to my old haunt \nshe's in my blood, she resides in my nightlife \nmy favorite color's candy apple girl");
 
-      message.channel.send(hEmbed);
+      message.channel.send({embed: hEmbed});
         //const data = [];
         //const {commands} = message.client;
-    },
-};
+}
+
+module.exports = {
+  name: 'help',
+  description: 'pls work'
+}
